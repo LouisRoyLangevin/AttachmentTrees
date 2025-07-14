4ba9222ff86655ba3470471871cf8fb931356649
LouisRoyLangevin <150217238+LouisRoyLangevin@users.noreply.github.com>
2025-07-14 11:53:45 -0400

    Added comments

--- RootFinding.cpp
+++ RootFinding.cpp
@@ -21,7 +21,7 @@ class Tree {
         vector<double> logPhi = {};      // (if phiToDate = true) i'th slot of this vector contains log(phi(i))
     public:
         Tree() = default;
-        Tree(vector<int> parents) {
+        Tree(vector<int> parents) {       // You can use this constructor if you want your tree to have an exact structure. logPhi will not be automatically updated!
             size = parents.size();
             parent = parents;
             children = vector<vector<int>>(size);
@@ -29,35 +29,35 @@ class Tree {
             phiToDate = false;
             logPhi = vector<double>(size);
         }
-        void add(int par) {      // Adds a vertex to the tree
+        void add(int par) {      // Adds a vertex to the tree that will have par as its parent
             parent.push_back(par);
             children.push_back({});
             children[par].push_back(size++);
             phiToDate = false;
         }
-        void updatePhi() {      // O(n) time algorithm for computing all values of Phi
+        void updatePhi() {      // O(n) time algorithm for computing all values of Phi, only uses one vector of length n of buffer memory
             if (size == 0) return;
             phiToDate = true;
-            vector<double> &sizeSubtree = logPhi;        // i'th slot of this vector contains the number of descendants of vertex i (including i) w.r.t the root
+            vector<double> &sizeSubtree = logPhi;        // i'th slot of this vector contains the number of descendants of vertex i (including i) w.r.t. the root
             for (int i = 0; i < size; i++) sizeSubtree[i] = 1;
             for (int i = size-1; i > 0; i--) {      // sizeSubtree[i] = 1 + \sum_{j is a child of i} sizeSubtree[j]
                 sizeSubtree[parent[i]] += sizeSubtree[i];
             }
 
-            vector<double> logProdSubtree;        // i'th slot of this vector contains the product of the sizes of all subtree of descendants of vertex i w.r.t the root
+            vector<double> logProdSubtree;        // i'th slot of this vector contains the product of the sizes of all subtrees of descendants of vertex i (including i) w.r.t. the root
             for (int k : sizeSubtree) logProdSubtree.push_back(log(k));
             for (int i = size-1; i > 0; i--) {       // logProdSubtree[i] = log(sizeSubtree[i]) + \sum_{j is a child of i} logProdSubtree[j]
                 logProdSubtree[parent[i]] += logProdSubtree[i];
             }
 
-            vector<double> &logProdSuptree = sizeSubtree;
+            vector<double> &logProdSuptree = sizeSubtree;       // i'th slot of this vector contains the product of the sizes of all subtrees needed to calculate phi(i) that aren't rooted at a descendant of i w.r.t. to root
             logProdSuptree[0] = 0.0;
-            for (int i = 1; i < size; i++) {
+            for (int i = 1; i < size; i++) {        // logProdSuptree[i] = log(size - sizeSubtree[i]) + logProdSuptree[parent[i]] - logProdSubtree[i] + \sum_{j is a child of parent[i]} logProdSubtree[j]
                 logProdSuptree[i] =  log(size - sizeSubtree[i]) + logProdSuptree[parent[i]] - logProdSubtree[i];
                 for (int j : children[parent[i]]) logProdSuptree[i] += logProdSubtree[j];
             }
 
-            for (int i = 0; i < size; i++) for (int j : children[i]) logPhi[i] += logProdSubtree[j];
+            for (int i = 0; i < size; i++) for (int j : children[i]) logPhi[i] += logProdSubtree[j];       // logPhi[i] = logProdSuptree[i] + \sum_{j is a child of i} logProdSubtree[j]
         }
         vector<double> getLogPhi() {
             return logPhi;
@@ -70,7 +70,7 @@ class Tree {
             }
             cout << "}" << endl;
         }
-        bool rootInTop(int k) {
+        bool rootInTop(int k) {       // Returns true if and only if there are at most k-1 vertices i other than 0 such that phi(i) <= phi(0)
             if (k >= size) return true;
             vector<double> tmp = logPhi;
             sort(tmp.begin(),tmp.end());
@@ -81,11 +81,10 @@ class Tree {
 
 class UATree : public Tree {
     public:
-        UATree(int n) {
+        UATree(int n) {     // Builds a tree of size n by uniform attachment. logPhi will not be automatically updated!
             for (int i = 0; i < n; i++) add();
-            updatePhi();
         }
-        void add() {
+        void add() {       // Adds a vertex by uniform attachment
             if (size == 0) parent.push_back(-1);
             else {
                 int par = randomInt(size-1);
@@ -107,6 +106,7 @@ double prob(ll K, int n, int N) {
     int cnt = 0;
     for (int i = 0; i < N; i++) {
         UATree tree = UATree(n);
+        tree.updatePhi();
         if (tree.rootInTop(K)) cnt++;
     }
     return ((double) cnt) / ((double) N);
